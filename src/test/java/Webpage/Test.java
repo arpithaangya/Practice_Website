9132fd956aaa8b201c6f3e3b33911b43a04a8d1b
Arpitha Shekhar Angya <arpitha.shekharangya@expleogroup.com>
2023-12-20 18:15:32 +0530

    changes done final 20th

--- src/test/java/Webpage/Test.java
+++ src/test/java/Webpage/Test.java
@@ -1,21 +1,40 @@
 package Webpage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.cucumber.java.After;
 
 public class Test {
 
-	
-	
 	public static void main(String[] args) {
 	
-		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.orangehrm.com/");
+		driver.get("https://omayo.blogspot.com/");
 		String title = driver.getTitle();
 		System.out.println("Title of webpage is "+title);
+		
+		WebElement read = driver.findElement(By.xpath("//div[@class=\"date-posts\"]/child::div//h3"));
+		System.out.println(read.getText());
+		
+		WebElement textarea = driver.findElement(By.id("ta1"));
+		textarea.sendKeys("This is sample text written by Arpitha");
+		
+		Select oldNews = new Select(driver.findElement(By.id("drop1")));
+		oldNews.selectByIndex(2);
+		
+		WebElement button = driver.findElement(By.id("but2"));
+		boolean checkbtn = button.isEnabled();
+		System.out.println("Button is enabled "+checkbtn);
+		
+		WebElement button1 = driver.findElement(By.id("but1"));
+		boolean checkbtn1 = button.isEnabled();
+		System.out.println("Button is enabled "+checkbtn1);
+		
+		
 	}
 	
 	
