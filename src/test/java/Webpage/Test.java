23e1f5e8b9eb90f0ecbb3cd81b73986ddcbfd474
Arpitha Shekhar Angya <arpitha.shekharangya@expleogroup.com>
2023-12-20 17:43:08 +0530

    arpi changes 20th

--- src/test/java/Webpage/Test.java
+++ src/test/java/Webpage/Test.java
@@ -1,5 +1,23 @@
 package Webpage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+
 public class Test {
 
+	
+	
+	public static void main(String[] args) {
+	
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.orangehrm.com/");
+		String title = driver.getTitle();
+		System.out.println("Title of webpage is "+title);
+	}
+	
+	
+	
 }
